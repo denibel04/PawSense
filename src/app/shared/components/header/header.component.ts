05c6f33912a833ad70f3232a3a7c90a41d1a32c5
CarlosCL2005 <235291712+Carloscl2005@users.noreply.github.com>
2026-03-02 21:27:38 +0100

    Modificando estilos

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,13 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IonHeader, IonToolbar, IonTitle } from '@ionic/angular/standalone';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, CommonModule]
+  imports: [IonHeader, IonToolbar, IonTitle, CommonModule, RouterLink]
 })
 export class HeaderComponent implements OnInit {
   constructor() { }
